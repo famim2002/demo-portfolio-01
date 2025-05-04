d96b4cc39e6e5ca50977df9f2a3c56b4b89f78c8
famim2002 <famimhayatyash@gmail.com>
2025-05-04 22:52:36 +0600

    alhamdulilah

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { Routes, Route, BrowserRouter } from "react-router";
 import About from "./pages/About";
 import Home from "./pages/Home";
+
 import Process from "./pages/Process";
 import Portfolio from "./pages/Portfolio";
 import Blog from "./pages/Blog";
 import Services from "./pages/Services";
-import Layout from "./components/home/Layout/Index"
+import Layout from "./components/home/Layout/Index";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/process" element={<Process />} />
@@ -19,7 +20,7 @@ function App() {
           <Route path="/blog" element={<Blog />} />
           <Route path="/services" element={<Services />} />
         </Route>
-         <Route path="/*" element={<Error/>} /> 
+        <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
